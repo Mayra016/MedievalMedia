382b99b1483d6b36982b98a70a64bbc8cdcef69a
Mayra016 <mayrazucaro@gmail.com>
2025-09-13 14:07:29 -0300

    create project basic structure

---
+++ src/test/java/com/MedievalMedia/MedievalMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MedievalMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedievalMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
