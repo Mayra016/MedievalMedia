2d004887e8808a2505cf19e99b6cd2ce7aca7496
Mayra016 <mayrazucaro@gmail.com>
2025-09-14 22:13:18 -0300

    Feature: add findAllByUserId in PostRepository

--- src/main/java/com/MedievalMedia/Repositories/PostRepository.java
+++ src/main/java/com/MedievalMedia/Repositories/PostRepository.java
@@ -1,11 +1,15 @@
 package com.MedievalMedia.Repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import com.MedievalMedia.Entities.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
+
+	List<Post> findAllByUserId(UUID userId);
 
 }
