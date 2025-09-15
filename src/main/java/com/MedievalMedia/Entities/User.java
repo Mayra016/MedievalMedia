1aadd77430cccfe9d76f43d12dc0a014f5bf96b0
Mayra016 <mayrazucaro@gmail.com>
2025-09-14 22:11:57 -0300

    Feature: update user id from long to UUID

--- src/main/java/com/MedievalMedia/Entities/User.java
+++ src/main/java/com/MedievalMedia/Entities/User.java
@@ -1,6 +1,10 @@
 package com.MedievalMedia.Entities;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import com.MedievalMedia.Enums.Language;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,18 +20,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
 	@Id
-	@GeneratedValue(strategy=GenerationType.UUID)
-	private long id;
+    @GeneratedValue
+    private UUID id;
 	private String email;
 	private String username;
 	private String password;
+	private List<Language> languages;
+	private Language appLanguage;
 	private LocalDate birthday;
 	private LocalDate enterDate;
 	private String country;
 	private String[] titles = new String[1];
 	
-	public User(String email, String username, String password, LocalDate birthday, String country, String title) {
+	public User(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+	
+	public User(String email, String username, String password, List<Language> languages, Language appLanguage, LocalDate birthday, String country, String title) {
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.languages = languages;
+		this.appLanguage = appLanguage;
+		this.birthday = birthday;
 		this.enterDate = LocalDate.now();
+		this.country = country;
 		titles[0] = title;
 	}
 
