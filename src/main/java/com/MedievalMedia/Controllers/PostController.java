2a03a970f9729e989a1d417a6cd09fbd0075a212
Mayra016 <mayrazucaro@gmail.com>
2025-09-17 19:36:38 -0300

    Feature: add /api/v1/delete-post endpoint to delete a post

--- src/main/java/com/MedievalMedia/Controllers/PostController.java
+++ src/main/java/com/MedievalMedia/Controllers/PostController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -47,6 +48,20 @@ public class PostController {
 		this.postRepository = postRepository;
 		this.userService = userService;
 		this.postService = postService;
+	}
+	
+	@DeleteMapping("/delete-post")
+	public ResponseEntity<String> deletePost(@RequestBody long postId) {
+		try {
+			this.postRepository.deleteById(postId);
+			
+			return ResponseEntity.status(HttpStatus.OK).body("Post was successful deleted");
+		} catch(Exception e) {
+			e.printStackTrace();
+			this.log.error("Error deleting post: " + postId);
+			
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting post");
+		}
 	}
 	
 	@PutMapping("/update-post")
