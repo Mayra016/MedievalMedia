957fb6ddd53e72d6d3457e48f3d2aa958ec3de32
Mayra016 <mayrazucaro@gmail.com>
2025-09-17 19:03:36 -0300

    Feature: add test to getPostsAnswers

--- src/test/java/com/MedievalMedia/Services/PostServiceTest.java
+++ src/test/java/com/MedievalMedia/Services/PostServiceTest.java
@@ -31,6 +31,7 @@ import com.MedievalMedia.Repositories.UserRepository;
 public class PostServiceTest {
 	private PostRepository postRepository;
 	private UserRepository userRepository;
+	private User user;
 
 	@Autowired
 	public PostServiceTest (PostRepository postRepository, UserRepository userRepository) {
@@ -42,7 +43,7 @@ public class PostServiceTest {
 
     @BeforeEach
     void initialize() {
-    	User user = this.userRepository.save(new User());
+    	user = this.userRepository.save(new User());
         for (int i = 0; i < 61; i++) {
         	Post post = new Post(user, "Greeting " + i, "Test content", "Spain", Language.ESPAÑOL);
         	post.setDate(LocalDate.now().minusDays(200-i));
@@ -81,6 +82,31 @@ public class PostServiceTest {
         
         assertEquals("Greeting 49", posts.get(0).getGreetings());
               
+    }
+    
+    
+    @Test
+    void getPostsAnswersTest() {
+    	Post parentPost = this.postRepository.findByGreetings("Greeting 2");
+    	Post answer1 = new Post(this.user, "Answer 1", "Nice letter", "England", Language.ENGLISH, parentPost);
+    	Post answer2 = new Post(this.user, "Answer 2", "Nice letter", "Spain", Language.ENGLISH, parentPost);
+    	Post answer3 = new Post(this.user, "Answer 3", "Nice letter", "Spain", Language.ENGLISH, parentPost);
+    	    	
+    	answer2.updateInteractions("crown", true);
+    	answer3.updateInteractions("heart", true);
+    	
+    	answer1 = this.postRepository.save(answer1);
+    	answer2 = this.postRepository.save(answer2);
+    	answer3 = this.postRepository.save(answer3);
+    	
+    	
+    	
+    	List<Post> posts = this.postRepository.findAllByParent(parentPost);
+    	System.out.println(" Interactions: " + answer2.getInteractions().getScore());
+    	System.out.println(" Size: " + posts.size());
+    	System.out.println(" First: " + posts.get(0));
+    	
+    	assertEquals(List.of(answer2, answer3, answer1), posts);
     }
 }
 
