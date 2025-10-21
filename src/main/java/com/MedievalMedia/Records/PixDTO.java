a9e9cc574f7d9d46a0816408a266262d00eebcda
Mayra016 <mayrazucaro@gmail.com>
2025-10-21 17:47:59 -0300

    SCRUM-23 FEAT add DTO objects to transfer payment information

--- src/main/java/com/MedievalMedia/Records/PixDTO.java
+++ src/main/java/com/MedievalMedia/Records/PixDTO.java
@@ -2,6 +2,6 @@ package com.MedievalMedia.Records;
 
 import java.util.Map;
 
-public record PixDAO(Map<String, String> qrcodePix, Map<String, String> valor, String chave, Map<String, String> devedor, RecurrencyPix recorrencia) {
+public record PixDTO(Map<String, String> qrcodePix, Map<String, String> valor, String chave, Map<String, String> devedor, RecurrencyPix recorrencia) {
 
 }
