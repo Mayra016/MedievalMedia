94681f133ef5d1c343517a2ad7ddf9acadc1730e
Mayra016 <mayrazucaro@gmail.com>
2025-11-03 16:37:54 -0300

    SCRUM-23 FEAT ADD findALlByIdAndStatus

--- src/main/java/com/MedievalMedia/Repositories/PaymentRepository.java
+++ src/main/java/com/MedievalMedia/Repositories/PaymentRepository.java
@@ -1,11 +1,19 @@
 package com.MedievalMedia.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.MedievalMedia.Entities.Payment;
+import com.MedievalMedia.Enums.Status;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, String>{
+
+	@Query("SELECT p FROM Payment p WHERE p.id IN :ids AND p.status = :status")
+	List<Payment> findAllByIdAndStatus(@Param("ids") List<String> ids, @Param("status") Status status);
 
 }
