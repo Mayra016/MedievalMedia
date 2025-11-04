fbd032bc0d3e35c38c0739c7cc7583d3d59cca9b
Mayra016 <mayrazucaro@gmail.com>
2025-11-04 18:40:39 -0300

    SCRUM-24 TEST added unit tests to withdraw, verification and authentication

--- src/test/java/com/MedievalMedia/Services/PayPalServiceTest.java
+++ src/test/java/com/MedievalMedia/Services/PayPalServiceTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URISyntaxException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,21 +15,75 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.MedievalMedia.Entities.User;
 import com.MedievalMedia.Records.PaymentRequest;
+import com.MedievalMedia.Repositories.PaymentRepository;
+import com.MedievalMedia.Repositories.UserRepository;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.PayPalRESTException;
 
-public class PayPalServiceTest {
-	private PaypalService service;
-	
 
+@DataJpaTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class PayPalServiceTest {
+	
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private PaymentRepository paymentRepository;
+	private PaypalService service;
+	private PaymentService paymentService;
+
+
+	@Value("${paypal.mode}") 
+	private String mode;
+	@Value("${paypal.client-id}") 
+	private String clientId;
+	@Value("${paypal.client-secret}") 
+	private String clientSecret;
+	@Value("${paypal.withdraw.url}")
+	private String withdrawUrl;
+	@Value("${paypal.balance.url}") 
+	String balanceUrl;
+	@Value("${paypal.auth.url}") 
+	String authUrl;
+	@Value("${paypal.email.client}") 
+	String emailTest;
+
+	
+	
 	@BeforeEach
 	void initialize() {
-		this.service = new PaypalService();		
+		this.paymentService = new PaymentService(this.paymentRepository);
+		this.service = new PaypalService(this.userRepository, this.clientId, this.clientSecret, this.mode, this.paymentService, this.withdrawUrl, this.balanceUrl, this.authUrl);
+	}
+	
+	@Test
+	public void withdrawPayment() throws ResponseStatusException, URISyntaxException, IOException, InterruptedException {
+		User user = new User();
+		user.setMoney(BigDecimal.valueOf(10));
+		user = userRepository.save(user);
+		
+		this.service.withdrawPayment(user.getId(), BigDecimal.valueOf(10), this.emailTest, "BRL");
+		
+		user = this.userRepository.findById(user.getId()).get();
+		
+		assertTrue(user.getMoney().compareTo(BigDecimal.valueOf(10)) == 0);
+	}
+	
+	@Test
+	public void verifyBalanceOK() throws URISyntaxException, IOException, InterruptedException {
+		String token = this.service.authenticate();
+		boolean result = this.service.verifyBalance(BigDecimal.valueOf(0), token, "BRL");
+
+		assertNotNull(token);
+		assertTrue(result);
 	}
 	
 	@Test
